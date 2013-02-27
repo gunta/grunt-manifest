422c013bdad0869cadadb8fa8196b42641f12523
Gunther Brunner <gunta85@gmail>
2013-02-27 17:16:24 +0900

    * Fixed jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,56 +8,58 @@
  */
 
 module.exports = function(grunt) {
-  'use strict';
+	'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: ['Gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>'],
-	  jshintrc: '.jshintrc'
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: ['Gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>'],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      test: ['tmp']
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			test: ['tmp']
+		},
 
-    // Configuration to be run (and then tested).
-    manifest: {
-      generate: {
-        options: {
-          basePath: 'test/fixtures',
-          timestamp: false
-        },
-        src: [
-          '*.js',
-          '*.css',
-          'folder_one/*',
-          'folder_two/*.js',
-          'folder_two/*.css'
-        ],
-        dest: 'tmp/manifest.appcache'
-      }
-    },
+		// Configuration to be run (and then tested).
+		manifest: {
+			generate: {
+				options: {
+					basePath: 'test/fixtures',
+					timestamp: false
+				},
+				src: [
+					'*.js',
+					'*.css',
+					'folder_one/*',
+					'folder_two/*.js',
+					'folder_two/*.css'
+				],
+				dest: 'tmp/manifest.appcache'
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
-  });
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // The clean plugin helps in testing.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// The clean plugin helps in testing.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Whenever the 'test' task is run, first clean the 'tmp' dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'manifest', 'nodeunit', 'clean']);
+	// Whenever the 'test' task is run, first clean the 'tmp' dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'manifest', 'nodeunit', 'clean']);
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
 };
